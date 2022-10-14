148d87638a697d44c8131ad3e87bc86fbadfd8b3
linzhi <lin_zhi201721@163.com>
2022-10-14 20:19:38 +0800

    搭建项目解构

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
   <div id="app">
-    根组件
+    <!-- 头部组件 -->
+    <Header />
+
+    <!-- 一级路由出口 -->
+    <router-view />
+
+    <!-- 底部组件 -->
+    <Footer />
   </div>
 </template>
 
-<style lang="less">
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
 
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style lang="less">
+// style 属性多了一个 lang 属性，值为 less
+// 这个 style 里面的代码按照 less 语法进行解析，就可以在 style 中写 less 代码
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入重置的样式文件
+import './assets/styles/reset.less'
+
 Vue.config.productionTip = false
 
 new Vue({
